03da838f6b94ae06627306c544fee00384b9bf98
Lysander <lysander6@gmail.com>
2022-12-31 19:38:17 +0100

    install marginalia for rich minibuffer items annotations

--- init.el
+++ init.el
@@ -191,3 +191,7 @@
   :custom
   (completion-styles '(orderless basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package marginalia
+  :ensure t
+  :init (marginalia-mode))
