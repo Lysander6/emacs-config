669031be845e67dc98a7700eccc405d486f84a82
Lysander <lysander6@gmail.com>
2023-01-01 17:49:38 +0100

    add `flex` back to `completion-styles`

--- init.el
+++ init.el
@@ -195,7 +195,7 @@
 (use-package orderless
   :ensure t
   :custom
-  (completion-styles '(orderless basic))
+  (completion-styles '(orderless flex basic))
   (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package marginalia
