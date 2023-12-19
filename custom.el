c07d716875b2ec3196ef8064decaa2ae9ec31d24
Lysander <lysander6@gmail.com>
2023-12-19 18:12:27 +0100

    Add custom.el

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cape copilot corfu-terminal diminish dimmer dirvish editorconfig
+          embark-consult evil-anzu evil-collection evil-commentary
+          evil-terminal-cursor-changer general git-gutter magit
+          marginalia markdown-mode nerd-icons-corfu orderless
+          shrink-path spaceline treemacs-evil treemacs-nerd-icons
+          vertico which-key winum yasnippet-capf yasnippet-snippets))
+ '(package-vc-selected-packages '((copilot :url "https://github.com/zerolfx/copilot.el"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
